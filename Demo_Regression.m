e1c4c88ccad563c6b7ce2f4520217d900f6baca0
XiaoweiGu <xw.gu@hotmail.com>
2020-04-22 15:54:53 +0100

    Update Demo_Regression.m

--- Demo_Regression.m
+++ Demo_Regression.m
@@ -2,29 +2,27 @@ clear all
 clc
 close all
 
-load ExampleDataset_regression
+load ExampleDataset_regression % load the example
 
-
+%% Define the interconnections between different layers (this is only an example used in the paper).
 L=length(X_training(1,:));
 seq=[1:1:L,1:1:L];
-count=1;
-for kk=1:1:4
-    ii=(kk-1)*floor(L/4)+1;
-    Layer{1}(count,:)=seq(ii:1:ii+ceil(L/2)-1);
-    count=count+1;
+for tt=1:1:4
+    ii=(tt-1)*floor(L/4)+1;
+    Layer{1}(tt,:)=seq(ii:1:ii+ceil(L/2)-1);
 end
 seq2=[1:1:length(Layer{1}(:,1)),1:1:length(Layer{1}(:,1))];
-for ii=1:1:4
+for ii=1:1:numElems2
     Layer{2}(ii,:)=seq2(ii:1:ii+length(Layer{1}(:,1))-2);
 end
 Layer{3}=[1:1:length(Layer{1}(:,1))];
-
-Setting.Layer=Layer;
-Setting.Epoch=1;
-Setting.FogettingFactor=0.1;
-Dataset.training=X_training;
-Dataset.traininglabels=Y_training;
-Dataset.testing=X_testing;
-[Output]=MEEFIS(Dataset,Setting);
+%% Define parameters.
+Setting.Layer=Layer; % Layers.
+Setting.Epoch=1; % Number of training epochs.
+Setting.ForgettingFactor=0.1; % Forgettingfactor.
+Dataset.training=X_training; % Training data.
+Dataset.traininglabels=Y_training;% Ground truth.
+Dataset.testing=X_testing; % Testing data.
+[Output]=MEEFIS(Dataset,Setting);  % Get the estimated output
 
 rmse=sqrt(mean((Output.Ye-Y_testing).^2))
