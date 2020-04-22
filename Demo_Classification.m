77db99c498198c2e631b2d97f7dc8a6806e3d913
XiaoweiGu <xw.gu@hotmail.com>
2020-04-22 15:48:09 +0100

    Update Demo_Classification.m

--- Demo_Classification.m
+++ Demo_Classification.m
@@ -1,31 +1,29 @@
 clear all
-% clc
+clc
 close all
 clear function
 
-load ExampleDataset_classification
-
+load ExampleDataset_classification % load the example
+%% Define the interconnections between different layers (this is only an example used in the paper).
 L=length(X_training(1,:));
 seq=[1:1:L,1:1:L];
-count=1;
 for tt=1:1:4
     ii=(tt-1)*floor(L/4)+1;
-    Layer{1}(count,:)=seq(ii:1:ii+ceil(L/2)-1);
-    count=count+1;
+    Layer{1}(tt,:)=seq(ii:1:ii+ceil(L/2)-1);
 end
 seq2=[1:1:length(Layer{1}(:,1)),1:1:length(Layer{1}(:,1))];
-for ii=1:1:4
+for ii=1:1:numElems2
     Layer{2}(ii,:)=seq2(ii:1:ii+length(Layer{1}(:,1))-2);
 end
 Layer{3}=[1:1:length(Layer{1}(:,1))];
-%%
-Setting.Layer=Layer;
-Setting.Epoch=1;
-Setting.FogettingFactor=0;
-Dataset.training=X_training;
-Dataset.traininglabels=full(ind2vec(Y_training')');
-Dataset.testing=X_testing;
-[Output]=MEEFIS(Dataset,Setting);
-[~,Output]=max(Output.Ye,[],2);
-confusionmat(Y_testing,Output)
-sum(sum(confusionmat(Y_testing,Output).*eye(length(unique(Y_testing)))))/length(Y_testing)
+%% Define parameters.
+Setting.Layer=Layer; % Layers.
+Setting.Epoch=1; % Number of training epochs.
+Setting.ForgettingFactor=0; % Forgettingfactor.
+Dataset.training=X_training; % Training data.
+Dataset.traininglabels=full(ind2vec(Y_training')'); % Ground truth.
+Dataset.testing=X_testing; % Testing data.
+[Output]=MEEFIS(Dataset,Setting); %% run meefis
+[~,Output]=max(Output.Ye,[],2); % Get the estimated output
+confusionmat(Y_testing,Output) % confusion matrix
+sum(sum(confusionmat(Y_testing,Output).*eye(length(unique(Y_testing)))))/length(Y_testing) % classification accuracy
